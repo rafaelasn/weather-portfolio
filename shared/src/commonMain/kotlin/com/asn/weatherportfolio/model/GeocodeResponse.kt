0552c6fa4e17db68152687bba66708f0d12f4af4
rafaelasn <rafael-asn@hotmail.com>
2023-11-24 17:55:57 -0400

    Serializable annotation

--- shared/src/commonMain/kotlin/com/asn/weatherportfolio/model/GeocodeResponse.kt
+++ shared/src/commonMain/kotlin/com/asn/weatherportfolio/model/GeocodeResponse.kt
@@ -1,15 +1,19 @@
 package com.asn.weatherportfolio.model
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class GeocodeResponse(
     @SerialName("features") var features: ArrayList<Features> = arrayListOf()
 ) {
+    @Serializable
     data class Features(
         @SerialName("type") var type: String? = null,
         @SerialName("properties") var properties: Properties? = Properties()
     )
 
+    @Serializable
     data class Properties(
         @SerialName("country") var country: String? = null,
         @SerialName("country_code") var countryCode: String? = null,
