7de5c772c3d77c57f54a2bcc45e5909c8588144e
rafaelasn <rafael-asn@hotmail.com>
2023-11-27 19:28:38 -0400

    logs added

--- shared/src/commonMain/kotlin/com/asn/weatherportfolio/repository/WeatherRepository.kt
+++ shared/src/commonMain/kotlin/com/asn/weatherportfolio/repository/WeatherRepository.kt
@@ -1,7 +1,9 @@
 package com.asn.weatherportfolio.repository
 
+import io.ktor.util.logging.KtorSimpleLogger
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
@@ -11,9 +13,13 @@ class WeatherRepository() {
 
     suspend fun getWeatherFromRemote(latitude: Double, longitude: Double) = flow {
         emit(api.getWeatherFromApi(latitude, longitude))
+    }.catch {
+        KtorSimpleLogger("getWeatherApi").debug("Unknown Error")
     }.flowOn(Dispatchers.IO)
 
     suspend fun getReverseGeocodingFromApi(latitude: Double, longitude: Double) = flow {
         emit(api.getReverseGeocodingFromApi(latitude, longitude))
+    }.catch {
+        KtorSimpleLogger("ReverseGeocodeApi").debug("Unknown Error")
     }.flowOn(Dispatchers.IO)
 }
